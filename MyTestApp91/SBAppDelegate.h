9207193560dd4c0e38c00df983fa452a060c507c
pooja.kamath <pooja.kamath@sourcebits.com>
2014-06-23 11:52:43 +0530

    Initial Commit

---
+++ MyTestApp91/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  MyTestApp91
+//
+//  Created by Pooja Kamath on 23/06/14.
+//  Copyright (c) 2014 Pooja Kamath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTestApp91/SBViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBViewController.h
+//  MyTestApp91
+//
+//  Created by Pooja Kamath on 23/06/14.
+//  Copyright (c) 2014 Pooja Kamath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBViewController : UIViewController
+
+@end
